e33511057f6896d1d561f310c35e8846b93a265b
r03eRt <morgadoluengo@gmail.com>
2016-10-16 21:23:33 +0200

    Added multiple sprites

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,8 +10,9 @@
 	    autoDetectRenderer = PIXI.autoDetectRenderer,
 	    loader = PIXI.loader,
 	    resources = PIXI.loader.resources,
+	    TextureCache = PIXI.utils.TextureCache,
+	    Texture = PIXI.Texture,
 	    Sprite = PIXI.Sprite;
-
 
 
 	// Create a renderer
@@ -26,27 +27,40 @@
 
 	// Load the resources and then init setup
 	loader
-		.add(baseUrl + 'cat.png')
+		.add(baseUrl + 'titleset.png')
 		.load(setup);
 
 
 	// Setup function will run when img loaded
 	function setup(){
 
-		// create the cat sprite from a texture
-		var cat = new Sprite(
-			resources[baseUrl + 'cat.png'].texture
-		);
+		// create the texture
+		var texture = TextureCache[baseUrl + 'titleset.png'];
+
+		//Create a rectangle object that defines the position and
+		//size of the sub-image you want to extract from the texture
+		var rectangle = new PIXI.Rectangle(192, 128, 64, 64);
+
+		// telkl the texture to use the rectangle section
+		console.log(texture);
+		texture.frame = rectangle;
+
+		// Now we can set the the sprite from the recture
+		var rocket = new Sprite(texture);
+		rocket.x = 32;
+	  rocket.y = 32;
+
+
 
 		// Set sprite position on stage
 		//cat.x = 96;
 		//cat.y = 96;
 		// Or sprite.position.set(x, y)
-		cat.position.set(96, 96)
+		//cat.position.set(96, 96)
 
 		// Set sprite width and height
-		cat.width = 120;
-		cat.height = 120;
+		//cat.width = 120;
+		//cat.height = 120;
 
 		// Set sprite scale
 		//cat.scale.x = 0.5;
@@ -55,18 +69,18 @@
 		//cat.scale.set(0.5, 0.5);
 
 		// Set opacity
-		cat.alpha = 0.5;
+		//cat.alpha = 0.5;
 
 
 		// Set rotation
-		cat.rotation =1;
+		//cat.rotation =1;
 
 		// Set anchor rotate .
 		// Center for example
 		//cat.anchor.x = 0.5;
 		//cat.anchor.y = 0.5;
 		// Or sprite.anchor.set(x, y)
-		cat.anchor.set(0.8, 0.8)
+		//cat.anchor.set(0.8, 0.8)
 
 
 		// Set transform origin on this case sprite 64x64
@@ -74,14 +88,14 @@
 
 
 		// Add the cat to the stage
-		stage.addChild(cat);
+		stage.addChild(rocket);
 
 		//Render the stage
 		renderer.render(stage);
 
 		var el = document.querySelector(".hide");
 
-		el.addEventListener('click', function(){
+	/**	el.addEventListener('click', function(){
 
 			if(this.classList.contains('hidden')){
 				this.classList.remove('hidden');
@@ -95,6 +109,7 @@
 
 
 		});
+		**/
 	}
 
 
