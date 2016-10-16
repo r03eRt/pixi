988f4197449773f8b8dfe58b919d2b536302693c
r03eRt <morgadoluengo@gmail.com>
2016-10-17 00:07:13 +0200

    Added velocity to explorer

--- assets/js/main.js
+++ assets/js/main.js
@@ -75,75 +75,92 @@
 		//cat.pivot.set(32, 32)
 
 
-	// Three types of charge texture from json
+		// Three types of charge texture from json
 
-	// Access directly to texture cache
-	var dungeonTexture = TextureCache["dungeon.png"];
-  dungeon = new Sprite(dungeonTexture);
-  stage.addChild(dungeon);
+		// Access directly to texture cache
+		var dungeonTexture = TextureCache["dungeon.png"];
+	  dungeon = new Sprite(dungeonTexture);
+	  stage.addChild(dungeon);
 
-	// Other form
-	//dungeon = new Sprite(resources[baseUrl + 'treasureHunter.json'].textures['dungeon.png']);
-	//stage.addChild(dungeon);
 
-	// Access with loader Resource
-	explorer = new Sprite(resources[baseUrl + 'treasureHunter.json'].textures['explorer.png']);
-	explorer.x = 68;
 
-	//Center the explorer vertically
-	explorer.y = stage.height / 2 - explorer.height / 2;
-	stage.addChild(explorer);
+		// Other form
+		//dungeon = new Sprite(resources[baseUrl + 'treasureHunter.json'].textures['dungeon.png']);
+		//stage.addChild(dungeon);
 
-	// Create a option alias id for avoid large naming
-	id = PIXI.loader.resources[baseUrl + "treasureHunter.json"].textures;
+		// Access with loader Resource
+		explorer = new Sprite(resources[baseUrl + 'treasureHunter.json'].textures['explorer.png']);
+		explorer.x = 68;
 
-	treasure = new Sprite(id['treasure.png']);
-	stage.addChild(treasure);
+		//Center the explorer vertically
+		explorer.y = stage.height / 2 - explorer.height / 2;
+		stage.addChild(explorer);
 
-	//Position the treasure next to the right edge of the canvas
-	treasure.x = stage.width - treasure.width - 48;
-	treasure.y = stage.height / 2 - treasure.height / 2;
-	stage.addChild(treasure);
+		explorer.vx = 0;
+		explorer.vy = 0;
 
-	// Added door to the stage
-	door = new Sprite(id['door.png']);
-	stage.addChild(door);
-	door.position.set(32, 0);
 
-	var numberOfBlobbs = 6,
-			spacing = 48,
-			xOffset = 150;
+		// Create a option alias id for avoid large naming
+		id = PIXI.loader.resources[baseUrl + "treasureHunter.json"].textures;
 
-	for (var i = 0; i < numberOfBlobbs; i++) {
+		treasure = new Sprite(id['treasure.png']);
+		stage.addChild(treasure);
 
-			// Make a bloob
-			var blob = new Sprite(id['blob.png']);
+		//Position the treasure next to the right edge of the canvas
+		treasure.x = stage.width - treasure.width - 48;
+		treasure.y = stage.height / 2 - treasure.height / 2;
+		stage.addChild(treasure);
 
-			//Space each blob horizontally according to the `spacing` value.
-	    //`xOffset` determines the point from the left of the screen
-	    //at which the first blob should be added.
-	    var x = spacing * i + xOffset;
+		// Added door to the stage
+		door = new Sprite(id['door.png']);
+		stage.addChild(door);
+		door.position.set(32, 0);
 
-			//Give the blob a random y position
-	    //(`randomInt` is a custom function - see below)
-	    var y = randomInt(0, stage.height - blob.height);
+		var numberOfBlobbs = 6,
+				spacing = 48,
+				xOffset = 150;
 
-			//Set the blob's position
-	    blob.x = x;
-	    blob.y = y;
+		for (var i = 0; i < numberOfBlobbs; i++) {
+				// Make a bloob
+				var blob = new Sprite(id['blob.png']);
+				//Space each blob horizontally according to the `spacing` value.
+		    //`xOffset` determines the point from the left of the screen
+		    //at which the first blob should be added.
+		    var x = spacing * i + xOffset;
+				//Give the blob a random y position
+		    //(`randomInt` is a custom function - see below)
+		    var y = randomInt(0, stage.height - blob.height);
+				//Set the blob's position
+		    blob.x = x;
+		    blob.y = y;
+		    //Add the blob sprite to the stage
+		    stage.addChild(blob);
+		}
+		// Render all elements
+		renderer.render(stage);
 
-	    //Add the blob sprite to the stage
-	    stage.addChild(blob);
-
+		// Starts the gameLoop
+		gameLoop();
 	}
 
 
+	function gameLoop(){
+
+		// Loops this game 60 times per second
+		requestAnimationFrame(gameLoop);
+
+		//Update explorer velocity
+		explorer.vx = 1;
+		explorer.vy = 1;
+
+		//Apply the velocity values to the cat's
+		//position to make it move
+		explorer.x += explorer.vx;
+		explorer.y += explorer.vy;
 
 
 
-	renderer.render(stage);
-
-
+		renderer.render(stage);
 
 	}
 
